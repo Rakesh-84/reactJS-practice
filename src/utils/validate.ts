087ea085c4cdc4c941918ba3fd68416ec290e510
Rakesh Das (Rex) <159632237+Rakesh-84@users.noreply.github.com>
2026-06-27 11:46:30 +0600

    update

---
+++ src/utils/validate.ts
@@ -0,0 +1,27 @@
+import {z, ZodError} from "zod";
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+
+function validateInput<T>(schema: z.ZodSchema<T>, rawInput: unknown): T {
+    const result = schema.safeParse(rawInput);
+    if (result.success) {
+        return result.data;
+    }
+    
+    throw new HttpException(
+        {
+            statusCode: 400,
+            message: "Validation failed",
+            errors: result.ZodError.errors.map((err) => ({
+                field: string,
+                messege:string {}
+            }))
+        },
+        HttpStatus.BAD_REQUEST
+
+
+
+}
+
+
+
