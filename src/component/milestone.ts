da0189fc583ca5c1db21ab4da590389c4d943b08
Rakesh Das (Rex) <159632237+Rakesh-84@users.noreply.github.com>
2026-06-27 11:27:44 +0600

    update

--- src/component/milestone.ts
+++ src/component/milestone.ts
@@ -1,21 +1,21 @@
 import {z} from "zod";
 
 export const milestoneSchema = z.object({
-    title: z.string().transform((val) => val.trim()).pipe(z.string().min(2)),
-    deadlineDate: z.string().transform((val) => new Date(val)).refine(date => new Date(date), {
+    title: z.string().transform((val) => val.trim()).pipe(z.string().min(2, {message: "title must be at least 2 characters long"})),
+    deadlineDate: z.string().transform((val) => new Date(val)).refine(date => new Date(), {
         message: "date will be used in future"}
     ),
-    assignedTo:z.string().uuid().optional()
+    assignedTo:z.string().uuid()
 });
 
-type createMilestoneInput = z.infer<typeof milestoneSchema>;
+type CreateMilestoneInput = z.infer<typeof milestoneSchema>;
 
-export class createMilestoneDto {
+export class CreateMilestoneDto {
     title: string;
     deadlineDate: Date;
     assignedTo?: string;
 
-constructor(data: createMilestoneInput) {
+constructor(data: CreateMilestoneInput) {
     this.title = data.title;
     this.deadlineDate = data.deadlineDate;
     this.assignedTo = data.assignedTo;
